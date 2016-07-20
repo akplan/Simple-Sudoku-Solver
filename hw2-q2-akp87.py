8e1d8b4cdd37e379f8cc115caabfa0b11e3ad661
akplan <alyssa.plan@gmail.com>
2016-07-20 12:10:57 -0400

    added formatting to printBoard, changed to numpy array but might change back to a list later.

--- hw2-q2-akp87.py
+++ hw2-q2-akp87.py
@@ -1,24 +1,31 @@
 # Homework 2: Simple Sudoku Solver with Local Search
 # Author: Alyssa Plan
+# Date: 07/20/2016
 
+# importing numpy for array functionality.
+import numpy
 
-# Function for printing the board as a grid.
-# Lists are very foreign to me still.
-def print_board(board):
-    for row in board:
-        for entry in row:
+def printBoard(board):
+    global horizontal_divider
+    horizontal_divider= '-------------'
+    print horizontal_divider
+    for i, row in enumerate(board):
+        print '|',
+        for j, entry in enumerate(row):
             print entry,
+            if(j == 1):
+                    print '|',
+        print '|',
         print
+        if(i == 1):
+            print horizontal_divider
+    print horizontal_divider
 
-# Main function. The lack of parenthesis still throws me off.
+# Main function. The lack of curly braces still throws me off.
 def main():
     # Initialize board with entries
-    simple_sudoku = [[1, 0, 3, 0], [0, 0, 0, 0], [2, 0, 4, 0], [0, 0, 0, 0]]
-
-    # Printing board for debug purposes, my python skills are
-    # hardly skills yet
-    # print "yarr how do i python"
-    print_board(simple_sudoku)
+    simple_sudoku = numpy.array([[1, 0, 3, 0], [0, 0, 0, 0], [2, 0, 4, 0], [0, 0, 0, 0]])
+    printBoard(simple_sudoku)
 
 
 main()
